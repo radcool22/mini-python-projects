99726fcacee750badc48e01aaa9ff41be591f842
radcool22 <kabirgupta2201@gmail.com>
2024-06-09 17:24:27 +0530

    simple changes made to GPA calculator

--- gpa_calculator.py
+++ gpa_calculator.py
@@ -1,6 +1,10 @@
 def main():
-    marks = input(("Enter your total marks achieved out of 100? "))
-    print("Your GPA is " + str(int(marks)/25))
+    marks = int(input(("Enter your total marks achieved out of 100? ")))
+    
+    if marks < 100:
+        print("Your GPA is " + str(marks/25))
+    else:
+        print("Please provide a value within a range of 100")
 
 if __name__ == "__main__":
     main()
